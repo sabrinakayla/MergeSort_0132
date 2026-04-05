f4d513bd3608d7189c7a7ef6e86afe3d98a0775e
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-05 12:17:08 +0700

    menambahkan fungsi merge sort

--- mergesort.cpp
+++ mergesort.cpp
@@ -32,3 +32,11 @@ void input()
         cin >> arr[i];
     }
 }
+
+// create function mergeSort
+void mergeSort(int low, int high)
+{
+    if (low >= high) // step 1
+    {
+        return; // step 1.a
+    }
